fb649f4ed9ac3392a16ae9087a1592297a86a0fb
blingbang <mattkrll@gmail.com>
2017-11-11 21:08:17 +0100

    added gen_trans_credentials, gen_key

--- keebreak.py
+++ keebreak.py
@@ -1,30 +1,71 @@
-import struct
 import binascii
-import hashlib
-import Crypto
+import struct
+from Crypto.Hash import SHA256
+from Crypto.Cipher import AES
 
 def gen_credentials(password):
-    creds_a = hashlib.sha256()
+    creds_a = SHA256.new()
     creds_a.update(password.encode("utf-8"))
 
-    creds_b = hashlib.sha256()
+    creds_b = SHA256.new()
     creds_b.update((creds_a.digest()))
 
     return creds_b.digest()
 
+def gen_trans_credentials(rounds, credentials, trans_seed, init_vector):
+
+    trans_creds = SHA256.new()
+    aes_cipher = AES.new(bytes(trans_seed), AES.MODE_ECB)
+    aes_a = aes_cipher.encrypt(credentials)
+
+    while True:
+        aes_a = aes_cipher.encrypt(aes_a)
+        rounds -= 1
+        if rounds == 0:
+            break
+
+    trans_creds.update(aes_a)
+
+    return trans_creds.digest()
+
+def gen_key(master_seed_hex, trans_creds_hex):
+
+    key_a = SHA256.new()
+    key_b = master_seed_hex
+    key_c = trans_creds_hex
+
+    key_a.update(key_b + key_c)
+
+    return key_a.digest()
+
+def decrypt(key, crypt_init_vector_hex, crypt_data_hex):
+
+    decrypt_cipher = AES.new(key, AES.MODE_CBC, crypt_init_vector_hex)
+
+    decrypt_a = decrypt_cipher.decrypt(crypt_data_hex)
+
+    return decrypt_a
+
+
 class KbdxHeader():
     def __init__(self, data):
         self.entries = []
+
         self._read_entry(data)
 
     def _read_entry(self, entry):
         e_id = entry[0]
         e_len = struct.unpack('<H', entry[1:3])[0]
         e_data = entry[3:e_len + 3]
+        e_cryptdata = entry[3:e_len + 31]
 
         if e_id != 0:
             self.entries.append((e_id, len(e_data), e_data))
             self._read_entry(entry[3 + e_len:])
+
+        if e_id == 0:
+            self.entries.append((e_id, len(e_cryptdata), e_cryptdata))
+
 
 with open('databases/Matthias_Kroell.kdbx', 'rb') as f:
     f_bytes = bytearray(f.read())
@@ -37,14 +78,13 @@ with open('databases/Matthias_Kroell.kdbx', 'rb') as f:
     master_seed = master_seed_tup[2]
     master_seed_hex = binascii.hexlify(master_seed)
 
-
     trans_seed_tup = header.entries[3]
     trans_seed_len = trans_seed_tup[1]
     trans_seed = trans_seed_tup[2]
     trans_seed_hex = binascii.hexlify(trans_seed)
 
     trans_rounds = (header.entries[4])[2]
-    trans_rounds_int = int.from_bytes(trans_rounds,byteorder='little')
+    trans_rounds_int = int.from_bytes(trans_rounds, byteorder='little')
 
     crypt_init_vector_tup = header.entries[5]
     crypt_init_vector_len = crypt_init_vector_tup[1]
@@ -56,16 +96,18 @@ with open('databases/Matthias_Kroell.kdbx', 'rb') as f:
     start_bytes = start_bytes_tup[2]
     start_bytes_hex = binascii.hexlify(start_bytes)
 
+    crypt_data_tup = header.entries[9]
+    crypt_data = crypt_data_tup[2]
+    crypt_data_hex = binascii.hexlify(crypt_data)
 
-
-#def gen_trans_credentials(credentials,trans_rounds,trans_seed):
-#    while rounds > 0:
-#        hashlib.
-
-
-#print(binascii.hexlify(gen_credentials("4567")))
 print(header.entries)
-print("\n","Masterseed:",master_seed_hex,"\n","Transformationseed:",trans_seed_hex,"\n","Transformation rounds:",trans_rounds_int)
-print('GenCreds:', binascii.hexlify(gen_credentials("1567")))
-print(crypt_init_vector_hex)
-print(start_bytes_hex)
+print("\n", "Masterseed:", master_seed_hex, "\n", "Transformationseed:", trans_seed_hex, "\n", "Transformation rounds:",
+      trans_rounds_int)
+print(' AES init vector', crypt_init_vector_hex)
+print(' First 32b decrypted data:', start_bytes_hex)
+print(' First 32b encrypted data:', crypt_data_hex)
+
+print(' GenCreds testoutput:', binascii.hexlify(gen_credentials("1111")))
+print(' GenTransCreds test:', binascii.hexlify(gen_trans_credentials(10000, gen_credentials("1111"), trans_seed, crypt_init_vector_hex)))
+print(' GenKey test:', binascii.hexlify(gen_key(master_seed_hex, gen_trans_credentials(10000, gen_credentials("1111"), trans_seed, crypt_init_vector_hex))))
+print('decrypt test: ')
